7eb1d515912ac092bb440ece451a87bfdc0bc917
www-data <www-data@braegelmann.net>
2011-11-06 12:18:16 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AndroidTranslationService::Application.config.secret_token = '04d07b59bec04711f0c3d032da893eb4b559d008b73d67da6da5c2969b6956c87b9b3cec35a8acb1a2e4ff67118c7baed76177c7d9b3b7fabd147bab2ef76e6d'
